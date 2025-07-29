5fcabcd72fd653ecc5f7edaf967c94a94267ff46
Nandan <167064786+Nandan206@users.noreply.github.com>
2025-07-29 19:27:48 +0530

    Time: 1 ms (99.86%), Space: 46.2 MB (60.27%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,15 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int sumofallelements = 0;
-        int sumofuniqueelements = 0;
+        int result = 0;
         for(int num : nums){
-            if(!set.contains(num)){
-                set.add(num);
-                sumofuniqueelements += num;
-            }
-            sumofallelements += num;
+            result ^= num;
         }
-        return 2 * sumofuniqueelements - sumofallelements;
+        return result;
     }
 }
